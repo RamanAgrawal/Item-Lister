225a29641dd5ee0052a8a8630e023e0789002c7a
Raman Agrawal <121716092+RamanAgrawal@users.noreply.github.com>
2023-02-07 13:50:09 +0530

    added filter method

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 // Form submit event
 form.addEventListener('submit', addItem);
@@ -53,4 +54,20 @@ function removeItem(e){
       itemList.removeChild(li);
     }
   }
+}
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
